a9362c5c3938a63962d98eff4b4994890ba620c6
joelorzet <joelorzt@gmail.com>
2022-11-23 21:32:19 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import './App.css';
 
 function App() {
 	const [count, setCount] = useState(0);
-	const a = 'ssss';
 	return (
 		<div className='App'>
 			<div>
-				<a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+				<a href='       https://vitejs.dev' target='_blank' rel='noreferrer'>
 					<img src='/vite.svg' className='logo' alt='Vite logo' />
 				</a>
-				<a href='https://reactjs.org' target='_blank' rel='noreferrer'>
+				<a
+					href='              https://reactjs.org'
+					target='_blank'
+					rel='noreferrer'>
 					<img src={reactLogo} className='logo react' alt='React logo' />
 				</a>
 			</div>
